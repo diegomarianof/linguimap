8380741b37748501e9b2d226b7f84be6437c5a1f
diegomarianof <dfernand@campus.ungs.edu.ar>
2025-08-24 20:43:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ function showLanguage(lang) {
   if (lang === "aleman") {
     content.innerHTML = `
       <h3>Alemán</h3>
-      <p>Aquí vas a poder declinar sustantivos, practicar vocabulario y explorar la gramática alemana.</p>
-    `;
+      <h3>Opciones para practicar Alemán</h3>
+      <ul>
+        <li>Artículos definidos</li>
+        <li>Artículos indefinidos</li>
+        <li>Pronombres posesivos</li>
+        <li>Pronombres reflexivos</li>
+        <li>Pronombres demostrativos</li>
+        <li>Desafío</li>
+      </ul>    `;
   } else if (lang === "ingles") {
     content.innerHTML = `
       <h3>Inglés</h3>
-      <p>Aquí vas a encontrar ejercicios de tiempos verbales, vocabulario y comprensión de frases en inglés.</p>
+      <p>Actividades para practicar inglés: EN CONSTRUCCIÓN.</p>
     `;
   } else {
     content.innerHTML = "<p>Selecciona un idioma para comenzar.</p>";
